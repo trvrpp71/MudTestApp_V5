bc4820a9d135b75e0de62984f959ef38d37d8120
trvrpp71 <tpublic@popesonline.com>
2022-05-18 15:18:16 -0500

    fixed test detail view iwth customer data

---
+++ Models/ViewModels/TestDetailViewModel.cs
@@ -0,0 +1,19 @@
+﻿
+using System.Collections.Generic;
+
+
+namespace MudTestApp.Models.TestViewModels
+{
+    public class TestDetailViewModel
+    {
+
+        public Test Test { get; set; }    //1 test
+
+        public Customer Customer { get; set; } // 1 customer
+
+        public IEnumerable<Compound>? Compounds { get; set; } //multiple compounds
+
+
+        public IEnumerable<TestResults>? TestResults { get; set; } //multiple tests
+    }
+}
