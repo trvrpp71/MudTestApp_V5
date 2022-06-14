8eb7ddd138029ff407600135bfb513021e0d63cc
trvrpp71 <tpublic@popesonline.com>
2022-06-14 16:40:58 -0500

    trying to get buttons working

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,8 +1,24 @@
 ﻿$(document).ready(function () {
     $('#indexTable').DataTable({
         "scrollY": "450px",
+        "scrollX": true,
         "scrollCollapse": true,
         "paging": true,
         "order": [[0,'desc']]
-    })
+    }),
+
+        $('#summaryTable').DataTable({
+            "paging": false,
+            "scrollY": 400
+        }),
+
+        $('#detailTable').DataTable({
+            dom: 'Bfrtip',
+            buttons: [
+                'copy', 'csv', 'excel', 'pdf', 'print'
+            ]
+        })
+
+
 });
+
