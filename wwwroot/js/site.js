6b9466896d00ba20aae4a9a44740766b4670c807
trvrpp71 <tpublic@popesonline.com>
2022-06-16 11:28:03 -0500

    got excel exports working; formated tables some more

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,23 +1,25 @@
 ﻿$(document).ready(function () {
+
     $('#indexTable').DataTable({
-        "scrollY": "450px",
+        "scrollY": "500px",
         "scrollX": true,
         "scrollCollapse": true,
         "paging": true,
         "order": [[0,'desc']]
     }),
 
-        $('#summaryTable').DataTable({
-            "paging": false,
-            "scrollY": 400
-        }),
+    $('#summaryTable').DataTable({
+        "paging": false,
+        "scrollY": 400,
+        dom: 'Bftr',
+        buttons: ['excel']
+    }),
 
-        $('#detailTable').DataTable({
-            dom: 'Bfrtip',
-            buttons: [
-                'copy', 'csv', 'excel', 'pdf', 'print'
-            ]
-        })
+    $('#detailTable').DataTable({
+        dom: 'Bftr',
+        buttons: ['excel'],
+        "paging":false
+    })
 
 
 });
